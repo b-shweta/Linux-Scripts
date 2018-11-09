5ed745940cfc7e8384cc500cdba3842c7203ec81
root <root@cc.lan>
2018-11-09 15:15:50 +0530

    website availability check with bashscript Please enter the commit message for your changes. Lines starting

---
+++ webstat/check_stat.sh
@@ -0,0 +1,22 @@
+#/bin/bash
+
+# script to check website URL is status (UP or DOWN)
+
+#while IFS='' read -r line || [[ -n "$line" ]]; 
+for line in `sed '/^$/d' $1`; 
+do
+
+echo "Hi : $line"
+ 
+curl -m 30 -LIs $line | grep "200 OK" > /dev/nul
+status=`echo $?`
+ if [ $status -ne 0 ];
+ then
+      web_stat=Down
+      echo $web_stat
+ else
+      web_stat=UP
+      echo $web_stat
+ fi
+
+done  
